2accf1603e5494f6f6f8b969d7b0be360bc50d6b
seter <seter1@mail.ru>
2023-11-02 11:58:50 +1200

    Update basic.py

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -5,6 +5,7 @@ from aiogram.filters import Command
 from aiogram import F
 from aiogram.types import ContentType
 from core.keyboards.reg_kb import reg_kb
+import emoji
 
 async def start_bot(bot: Bot):
     await bot.send_message(settings.bots.admin_id,text='Бот запущен.')
@@ -13,8 +14,9 @@ async def stop_bot(bot:Bot):
     await bot.send_message(settings.bots.admin_id,text='Бот остановлен.')
 
 async def get_start(message: Message):
-    await message.answer(f'Привет! Этот бот служит для получения и выполнения дополнительных заданий '
-                         f'по физике. Прежде чем использовать бота необходимо зарегистрироваться.',
+    await message.answer(emoji.emojize('Привет! Этот бот служит для получения и выполнения дополнительных заданий '
+                         f'по физике. Прежде чем использовать бота необходимо зарегистрироваться.'
+                         f' Нажмите на кнопку "/reg" :backhand_index_pointing_down:'),
                          reply_markup=reg_kb)
 
 async def get_image(message: Message, bot: Bot):
