c210dac8d593675c1fcb7b719fade36cc17af95b
seter <seter1@mail.ru>
2023-11-02 10:21:07 +1200

    Create register_client.py

---
+++ core/handlers/register_client.py
@@ -0,0 +1,15 @@
+from aiogram import Dispatcher,F
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from utils.states import StepsReg
+from aiogram.filters import Command
+from aiogram.types import ContentType
+
+
+async def reg_start(message:Message,state:FSMContext):
+    await message.answer(f'Начинаем регистрацию. Введите свое имя.')
+    await state.set_state(StepsReg.GET_NAME)
+
+
+def register_reg_client(dp: Dispatcher):
+    dp.message.register(reg_start, 'Регистрация')
