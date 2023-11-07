92334dce94131387953218492427b54ffbe29b38
seter <seter1@mail.ru>
2023-11-07 17:33:15 +1200

    Update db_class.py

--- utils/db_class.py
+++ utils/db_class.py
@@ -5,4 +5,15 @@ class DataBase:
         self.connect=sqlite3.connect(file)
         self.cursor=self.connect.cursor()
 
-    
+    def create_table(self):
+        with self.connect as db:
+            query="""
+            CREATE TABLE IF NOT EXISTS clients(
+            id INTEGER PRIMARY KEY,
+            id_tel VARCHAR(10),
+            name VARCHAR(30),
+            last_name VARCHAR(30)
+            klass VARCHAR(3)
+            )
+            """
+            self.cursor.executescript(query)
