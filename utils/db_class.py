f5422a7280e7853ba710d318c77be77e6f24901d
seter <seter1@mail.ru>
2023-11-09 15:45:33 +1200

    Update db_class.py

--- utils/db_class.py
+++ utils/db_class.py
@@ -6,14 +6,28 @@ class DataBase:
         self.cursor=self.connect.cursor()
 
     def create_table(self):
-        with self.connect as db:
+        with self.connect:
             query="""
             CREATE TABLE IF NOT EXISTS clients(
             id INTEGER PRIMARY KEY,
             id_tel VARCHAR(10),
             name VARCHAR(30),
-            last_name VARCHAR(30)
+            last_name VARCHAR(30),
             klass VARCHAR(3)
             )
             """
             self.cursor.executescript(query)
+
+    def client_exists(self, client_id_tel):
+        with self.connect:
+            result = self.cursor.execute("SELECT id_tel FROM clients WHERE id_tel=?", (client_id_tel,)).fetchone()
+            if result:
+                return True
+            else:
+                return False
+
+    # добавление клиента в базу
+    def add_client(self, client_id_tel, client_name, client_last_name, client_klass):
+        with self.connect:
+           val = [client_id_tel, client_name, client_last_name, client_klass]
+           self.cursor.execute("INSERT INTO clients(id_tel, name, last_name, klass) VALUES(?,?,?,?)", val)
