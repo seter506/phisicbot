02da78ca1b5b00f22cd1bfd723e30756896d8404
seter <seter1@mail.ru>
2023-11-13 16:31:24 +1200

    Update basic.py

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -6,6 +6,7 @@ from aiogram import F
 from aiogram.types import ContentType
 from core.keyboards.reg_kb import reg_kb
 import emoji
+from utils.db_class import DataBase
 
 async def start_bot(bot: Bot):
     await bot.send_message(settings.bots.admin_id,text='Бот запущен.')
@@ -14,10 +15,17 @@ async def stop_bot(bot:Bot):
     await bot.send_message(settings.bots.admin_id,text='Бот остановлен.')
 
 async def get_start(message: Message):
-    await message.answer(emoji.emojize('Привет! Этот бот служит для получения и выполнения дополнительных заданий '
-                         f'по физике. Прежде чем использовать бота необходимо зарегистрироваться.'
-                         f' Нажмите на кнопку "/reg" :backhand_index_pointing_down:'),
-                         reply_markup=reg_kb)
+    db=DataBase('utils/database.db')
+    if db.client_exists(message.from_user.id):
+        if db.client_confirm(message.from_user.id):
+            await message.answer(f'Вы зарегистрированы и подтверждены.')
+        else:
+            await message.answer(f'Вы зарегистрированы, подождите подтверждение вашей заявки.')
+    else:
+        await message.answer(emoji.emojize('Привет! Этот бот служит для получения и выполнения дополнительных заданий '
+                                           f'по физике. Прежде чем использовать бота необходимо зарегистрироваться.'
+                                           f' Нажмите на кнопку "/reg" :backhand_index_pointing_down:'),
+                             reply_markup=reg_kb)
 
 async def get_image(message: Message, bot: Bot):
     await message.answer(f'Сохраняю картинку...')
