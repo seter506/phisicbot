9f338475354ad6f28eed850f19760d615e48b0d3
seter <seter1@mail.ru>
2023-11-20 18:19:09 +1200

    Update admin.py

--- core/handlers/admin.py
+++ core/handlers/admin.py
@@ -3,6 +3,7 @@ from aiogram.types import Message, InlineKeyboardButton,InlineKeyboardMarkup
 from core.keyboards.admin_kb import admin_menu_kb
 from aiogram.filters import Command
 from utils.db_class import DataBase
+from core.handlers.callback import client_confirm
 
 async def get_adm_menu(message: Message):
     await message.answer(f'Вывожу меню.', reply_markup=admin_menu_kb)
@@ -13,8 +14,8 @@ async def get_unconf_clients(message: Message):
     await message.answer(f'Вывожу список.')
     await message.answer('*** НАЧАЛО СПИСКА ***')
     for i in cl:
-        inl_ok = InlineKeyboardButton(text='Подтвердить', callback_data=f'ok {i[0]}')
-        inl_can = InlineKeyboardButton(text='Отменить', callback_data=f'cancel {i[0]}')
+        inl_ok = InlineKeyboardButton(text='Подтвердить', callback_data=f'call_ok_{i[0]}')
+        inl_can = InlineKeyboardButton(text='Отменить', callback_data=f'call_cancel_{i[0]}')
         inl_kb = InlineKeyboardMarkup(inline_keyboard=[[inl_ok,inl_can]])
         await message.answer(
             f'Ученик №{i[0]}.\nИмя: {i[1]}\nФамилия: {i[2]}\nКласс: {i[3]}', reply_markup=inl_kb)
@@ -25,3 +26,4 @@ async def get_unconf_clients(message: Message):
 def register_admin(dp: Dispatcher):
     dp.message.register(get_adm_menu, Command(commands=['admin_menu']))
     dp.message.register(get_unconf_clients, F.text=='заявки')
+    dp.callback_query.register(client_confirm, F.data.startswith('call_'))
