8f669786a1d68d76d22484ef84ae569d9c1d2413
seter <seter1@mail.ru>
2023-11-21 17:58:45 +1200

    Update admin.py

--- core/handlers/admin.py
+++ core/handlers/admin.py
@@ -14,8 +14,8 @@ async def get_unconf_clients(message: Message):
     await message.answer(f'Вывожу список.')
     await message.answer('*** НАЧАЛО СПИСКА ***')
     for i in cl:
-        inl_ok = InlineKeyboardButton(text='Подтвердить', callback_data=f'call_ok_{i[0]}')
-        inl_can = InlineKeyboardButton(text='Отменить', callback_data=f'call_cancel_{i[0]}')
+        inl_ok = InlineKeyboardButton(text='Подтвердить', callback_data=f'conf_ok_{i[0]}_{i[4]}')
+        inl_can = InlineKeyboardButton(text='Отменить', callback_data=f'conf_cancel_{i[0]}_{i[4]}')
         inl_kb = InlineKeyboardMarkup(inline_keyboard=[[inl_ok,inl_can]])
         await message.answer(
             f'Ученик №{i[0]}.\nИмя: {i[1]}\nФамилия: {i[2]}\nКласс: {i[3]}', reply_markup=inl_kb)
@@ -26,4 +26,4 @@ async def get_unconf_clients(message: Message):
 def register_admin(dp: Dispatcher):
     dp.message.register(get_adm_menu, Command(commands=['admin_menu']))
     dp.message.register(get_unconf_clients, F.text=='заявки')
-    dp.callback_query.register(client_confirm, F.data.startswith('call_'))
+    dp.callback_query.register(client_confirm, F.data.startswith('conf_'))
