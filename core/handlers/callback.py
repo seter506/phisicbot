b8701ac275ff3eaaadf41f37ed01c95b4ef76ec0
seter <seter1@mail.ru>
2023-11-21 17:58:37 +1200

    Update callback.py

--- core/handlers/callback.py
+++ core/handlers/callback.py
@@ -1,10 +1,19 @@
 from aiogram import Bot
 from aiogram.types import CallbackQuery
+from utils.db_class import DataBase
 
 async def client_confirm(call: CallbackQuery, bot: Bot):
     conf=call.data.split('_')[1]
     client=call.data.split('_')[2]
-    answer=f'Была нажата кнопка с параметрами: {conf} - {client}'
-    await call.answer(answer)
-    await call.answer()
+    id_tel=call.data.split('_')[3]
+    print(conf,client,id_tel)
+    db=DataBase('utils/database.db')
+    if conf=='ok':
+        db.conf_ok_client(id_tel)
+        await call.answer(f'Ученик под номером {client} подтвержден.')
+        await bot.send_message(id_tel,text='Ваша заявка подтверждена.')
+    else:
+        db.cursor.execute('DELETE FROM clients WHERE id_tel=?',(id_tel,))
+        await call.answer(f'Заявка ученика {client} отклонена.')
+        await bot.send_message(id_tel,text='Ваша заявка отклонена. Попробуйте еще раз.')
 
